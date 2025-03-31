d228ded823a99e633203f15fa83235a60272478b
Luis Arias <arias-22@outlook.com>
2025-03-31 11:52:08 -0600

    feat: set basic app config with params

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { onMounted } from 'vue';
 import Home from './components/Home.vue'
+
+onMounted(() => {
+  document.title = 'Page name';
+  document.documentElement.lang = 'es';
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.href = 'https://primefaces.org/cdn/primevue/images/landing/features/icon-mobile.svg'; 
+  document.head.appendChild(link);
+});
 </script>
 
 <template>
